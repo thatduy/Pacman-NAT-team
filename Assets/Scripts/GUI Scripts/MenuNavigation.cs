27160098ade57991d87f956ab9adba2824b5ee3e
VDT\ASUS <vdt.uit.studio@gmail.com>
2017-06-02 20:45:37 +0700

    Kick off

---
+++ Assets/Scripts/GUI Scripts/MenuNavigation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuNavigation : MonoBehaviour {
+
+	public void MainMenu()
+	{
+		Application.LoadLevel("menu");
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
+	
+	public void Play()
+	{
+		Application.LoadLevel("game");
+	}
+	
+	public void HighScores()
+	{
+		Application.LoadLevel("scores");
+		
+	}
+		
+
+
+}
